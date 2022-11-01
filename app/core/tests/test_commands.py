a53417604d70371fc5d4abbc5d72966b71b41b8b
Leandro Almeida de Araujo <oxydated@hotmail.com>
2022-11-01 14:11:27 -0300

    Linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """"Test commands"""
@@ -34,3 +35,4 @@ class CommandTests(SimpleTestCase):
         self.assertEqual(patched_check.call_count, 6)
 
         patched_check.assert_called_with(databases=['default'])
+        
