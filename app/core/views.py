a775b03a1626adf1eb0cf541ac768ad40686f94d
Leandro Almeida de Araujo <oxydated@hotmail.com>
2023-07-23 04:28:41 -0300

    upgrading DJANGO

--- app/core/views.py
+++ app/core/views.py
@@ -5,6 +5,7 @@ Core views for app.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def health_check(request):
     """REturns successful response."""
